d5fb755f57441d108d6eba19fed441a34cdb1adc
Akbar Khaydarkhujayev <U0057@so.local>
2025-04-28 12:34:51 +0500

    task5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "./components/theme-provider";
 import { Toaster } from "./components/ui/sonner";
-import UserTable from "./pages/lesson5/part2";
+import UserTable from "./pages/lesson5";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
